f1983dae9989d36e8155bad1ceda5057526bbf48
pansong291 <pansong291@sina.com>
2023-01-06 00:00:59 +0800

    feat: 新窗链接增加图标

--- assets/js-portal2/define-link-target-blank.js
+++ assets/js-portal2/define-link-target-blank.js
@@ -1,4 +1,4 @@
 $(() => {
-    const $links = $('#core-wrapper .post-content a[href]:not(.anchor):not([target])')
-    $links.attr('target', '_blank')
+    $('#core-wrapper .post-content a[href][target=_blank]:not(.anchor)').append('<i class="fa fa-external-link ml-1"/>')
+    $('#core-wrapper .post-content a[href]:not(.anchor):not([target])').attr('target', '_blank').append('<i class="fa fa-external-link ml-1"/>')
 })
