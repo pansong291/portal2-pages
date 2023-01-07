fc586e54374a3eb77e583eb3e3f1d35e7ad62f52
pansong291 <pansong291@sina.com>
2023-01-07 15:40:03 +0800

    fix: 排除一些页内导航链接

--- assets/js-portal2/define-link-target-blank.js
+++ assets/js-portal2/define-link-target-blank.js
@@ -1,4 +1,4 @@
 $(() => {
-    $('#core-wrapper .post-content a[href][target=_blank]:not(.anchor)').append('<i class="fa fa-external-link ml-1"/>')
-    $('#core-wrapper .post-content a[href]:not(.anchor):not([target])').attr('target', '_blank').append('<i class="fa fa-external-link ml-1"/>')
+    $('#core-wrapper .post-content a[href]:not([target],[href^="#"],.anchor,.footnote,.reversefootnote)').attr('target', '_blank')
+    $('#core-wrapper .post-content a[href][target=_blank]').append('<i class="fa fa-external-link ml-1"/>')
 })
