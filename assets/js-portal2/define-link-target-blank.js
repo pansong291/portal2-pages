05148b4583ae8fb12deb3e9c96bd0f3cd430bee3
pansong291 <pansong291@sina.com>
2023-01-21 23:36:25 +0800

    fix: 排除图片链接

--- assets/js-portal2/define-link-target-blank.js
+++ assets/js-portal2/define-link-target-blank.js
@@ -1,4 +1,4 @@
 $(() => {
-    $('#core-wrapper .post-content a[href]:not([target],[href^="#"],.anchor,.footnote,.reversefootnote)').attr('target', '_blank')
+    $('#core-wrapper .post-content a[href]:not([target],[href^="#"],.anchor,.footnote,.reversefootnote,.img-link)').attr('target', '_blank')
     $('#core-wrapper .post-content a[href][target=_blank]').append('<i class="fa fa-external-link ml-1"/>')
 })
